93a8a2cda217d387fa0b6dd108c761aa3ac473fd
Tim Glabisch <tim.glabisch@sensiolabs.de>
2015-11-06 18:30:32 +0100

    just test the aspMapGenerator

--- src/Tests/Visitor/AstMapGeneratorTest.php
+++ src/Tests/Visitor/AstMapGeneratorTest.php
@@ -18,22 +18,19 @@ use DependencyTracker\Tests\Visitor\Fixtures\BasicDependency\BasicDependencyTrai
 use DependencyTracker\Tests\Visitor\Fixtures\BasicDependency\BasicDependencyTraitC;
 use DependencyTracker\Tests\Visitor\Fixtures\BasicDependency\BasicDependencyTraitClass;
 use DependencyTracker\Tests\Visitor\Fixtures\BasicDependency\BasicDependencyTraitD;
-use DependencyTracker\Visitor\BasicDependencyVisitor;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\Finder\Finder;
 
-class BasicDependencyVisitorTest extends \PHPUnit_Framework_TestCase
+class AstMapGeneratorTest extends \PHPUnit_Framework_TestCase
 {
     use ArrayAsserts;
 
     /**
      * @param $fixture
-     * @return DependencyResult
+     * @return AstMap
      */
     private function getDependencyResultForFixture($fixture)
     {
-        $dependencyResult = new DependencyResult();
-
         $files = iterator_to_array(
             (new Finder())->in(__DIR__.'/Fixtures/BasicDependency/')->name($fixture.'.php')->files()
         );
@@ -43,77 +40,51 @@ class BasicDependencyVisitorTest extends \PHPUnit_Framework_TestCase
             $files
         );
 
-        (new BasicDependencyVisitor($dependencyResult))->analyze($astMap);
-
-        return $dependencyResult;
+        return $astMap;
     }
 
     public function testBasicDependencyClass()
     {
-        $dependencyResult = $this->getDependencyResultForFixture('BasicDependencyClass');
+        $astMap = $this->getDependencyResultForFixture('BasicDependencyClass');
 
         $this->assertArrayValuesEquals(
             [BasicDependencyClassA::class, BasicDependencyClassInterfaceA::class],
-            $this->getDepsForClass(BasicDependencyClassB::class, $dependencyResult)
+            $astMap->getClassInherits(BasicDependencyClassB::class, $astMap)
         );
 
         $this->assertArrayValuesEquals(
             [BasicDependencyClassInterfaceA::class, BasicDependencyClassInterfaceB::class],
-            $this->getDepsForClass(BasicDependencyClassC::class, $dependencyResult)
+            $astMap->getClassInherits(BasicDependencyClassC::class, $astMap)
         );
     }
 
     public function testBasicTraitsClass()
     {
-        $dependencyResult = $this->getDependencyResultForFixture('BasicDependencyTraits');
+        $astMap = $this->getDependencyResultForFixture('BasicDependencyTraits');
 
         $this->assertArrayValuesEquals(
             [],
-            $this->getDepsForClass(BasicDependencyTraitA::class, $dependencyResult)
+            $astMap->getClassInherits(BasicDependencyTraitA::class)
         );
 
         $this->assertArrayValuesEquals(
             [],
-            $this->getDepsForClass(BasicDependencyTraitB::class, $dependencyResult)
+            $astMap->getClassInherits(BasicDependencyTraitB::class)
         );
 
         $this->assertArrayValuesEquals(
             [BasicDependencyTraitB::class],
-            $this->getDepsForClass(BasicDependencyTraitC::class, $dependencyResult)
+            $astMap->getClassInherits(BasicDependencyTraitC::class)
         );
 
         $this->assertArrayValuesEquals(
             [BasicDependencyTraitA::class, BasicDependencyTraitB::class],
-            $this->getDepsForClass(BasicDependencyTraitD::class, $dependencyResult)
+            $astMap->getClassInherits(BasicDependencyTraitD::class)
         );
 
         $this->assertArrayValuesEquals(
             [BasicDependencyTraitA::class],
-            $this->getDepsForClass(BasicDependencyTraitClass::class, $dependencyResult)
-        );
-    }
-
-
-    private function getDepsForClass($class, DependencyResult $dependencyResult)
-    {
-        $deps = array_filter(
-            $dependencyResult->getDependencies(),
-            function (DependencyResult\Dependency $v) use ($class) {
-                if ($class !== $v->getClassA()) {
-                    return false;
-                }
-
-                return !($v instanceof DependencyResult\InheritDependency);
-            }
-        );
-
-        return array_values(
-            array_map(
-                function (DependencyResult\Dependency $dependency) {
-                    return $dependency->getClassB();
-                },
-                $deps
-            )
+            $astMap->getClassInherits(BasicDependencyTraitClass::class)
         );
     }
 }
