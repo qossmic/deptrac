4f3a5b4ff5c67adaa11fd4c877fe5f1119e81e09
Tim Glabisch <tim.glabisch@sensiolabs.de>
2016-02-17 20:29:54 +0100

    + coverage

--- src/Tests/ClassNameLayerResolverTest.php
+++ src/Tests/ClassNameLayerResolverTest.php
@@ -4,10 +4,114 @@
 namespace DependencyTracker\Tests;
 
 
+use DependencyTracker\ClassNameLayerResolver;
+use DependencyTracker\Collector\CollectorInterface;
+use DependencyTracker\CollectorFactory;
+use DependencyTracker\Configuration;
+use DependencyTracker\Configuration\ConfigurationCollector;
+use DependencyTracker\Configuration\ConfigurationLayer;
+use Prophecy\Argument;
+use SensioLabs\AstRunner\AstMap;
+use SensioLabs\AstRunner\AstParser\AstClassReferenceInterface;
+use SensioLabs\AstRunner\AstParser\NikicPhpParser\AstClassReference;
+
 class ClassNameLayerResolverTest extends \PHPUnit_Framework_TestCase
 {
-    public function testFoo()
+    private function getCollectorConfiguration($type)
     {
-        
+        $collectorConfiguration = $this->prophesize(ConfigurationCollector::class);
+        $collectorConfiguration->getType()->willReturn($type);
+        return $collectorConfiguration->reveal();
+    }
+
+    private function getCollector($return)
+    {
+        $collector = $this->prophesize(CollectorInterface::class);
+        $collector->satisfy(
+            Argument::type('array'),
+            Argument::type(AstClassReferenceInterface::class),
+            Argument::type(AstMap::class),
+            Argument::type(CollectorFactory::class)
+        )->willReturn($return);
+
+        return $collector->reveal();
+    }
+
+    public function provideGetLayersByClassName()
+    {
+        yield [
+            1, 1, 1, ['LayerA', 'LayerB']
+        ];
+
+        yield [
+            1, 0, 1, ['LayerA', 'LayerB']
+        ];
+
+        yield [
+            0, 0, 1, ['LayerB']
+        ];
+
+        yield [
+            1, 1, 0, ['LayerA', 'LayerB']
+        ];
+
+        yield [
+            1, 0, 0, ['LayerA']
+        ];
+
+
+        yield [
+            0, 0, 0, []
+        ];
+    }
+
+    /**
+     * @param $collectA
+     * @param $collectB1
+     * @param $collectB2
+     * @param array $expectedLayers
+     * @dataProvider provideGetLayersByClassName
+     */
+    public function testGetLayersByClassName($collectA, $collectB1, $collectB2, array $expectedLayers)
+    {
+        $configuration = $this->prophesize(Configuration::class);
+        $configuration->getLayers()->willReturn([
+            ConfigurationLayer::fromArray([
+                'name' => 'LayerA',
+                'collectors' => [
+                    ['type' => 'CollectorA']
+                ]
+            ]),
+            ConfigurationLayer::fromArray([
+                'name' => 'LayerB',
+                'collectors' => [
+                    ['type' => 'CollectorB1'],
+                    ['type' => 'CollectorB2']
+                ]
+            ]),
+        ]);
+
+        $astMap = $this->prophesize(AstMap::class);
+        $collectorFactory = $this->prophesize(CollectorFactory::class);
+        $collectorFactory->getCollector('CollectorA')->willReturn(
+            $this->getCollector($collectA, ['type' => 'CollectorA', 'foo' => 'bar'])
+        );
+        $collectorFactory->getCollector('CollectorB1')->willReturn(
+            $this->getCollector($collectB1, ['type' => 'CollectorB', 'foo' => 'bar'])
+        );
+        $collectorFactory->getCollector('CollectorB2')->willReturn(
+            $this->getCollector($collectB2, ['type' => 'CollectorB', 'foo' => 'bar'])
+        );
+
+        $resolver = new ClassNameLayerResolver(
+            $configuration->reveal(),
+            $astMap->reveal(),
+            $collectorFactory->reveal()
+        );
+
+        $this->assertEquals(
+            $expectedLayers,
+            $resolver->getLayersByClassName('classA')
+        );
     }
 }
