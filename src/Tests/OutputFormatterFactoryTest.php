5acc13c5c13729c6c9bcf51f21b4eda5754f2757
Tim Glabisch <tim.glabisch@sensiolabs.de>
2016-03-11 13:31:54 +0100

    + tests

---
+++ src/Tests/OutputFormatterFactoryTest.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace DependencyTracker\Tests;
+
+
+use DependencyTracker\OutputFormatterFactory;
+use DependencyTracker\OutputFormatter\OutputFormatterInterface;
+
+class OutputFormatterFactoryTest extends \PHPUnit_Framework_TestCase
+{
+
+    private function createNamedFormatter($name)
+    {
+        $formatter = $this->prophesize(OutputFormatterInterface::class);
+        $formatter->getName()->willReturn($name);
+
+        return $formatter->reveal();
+    }
+
+    public function testGetFormatterByName()
+    {
+        $formatterFactory = new OutputFormatterFactory([
+            $formatter1 = $this->createNamedFormatter('formatter1'),
+            $formatter2 = $this->createNamedFormatter('formatter2')
+        ]);
+
+        $this->assertSame($formatter1, $formatterFactory->getFormatterByName('formatter1'));
+        $this->assertSame($formatter2, $formatterFactory->getFormatterByName('formatter2'));
+    }
+
+    /**
+     * @expectedException \LogicException
+     */
+    public function testGetFormatterByNameNotFound()
+    {
+        (new OutputFormatterFactory([]))->getFormatterByName('formatter1');
+    }
+
+}
