6f3d799236271f68c520f2c862cf21b2eb8d2511
Tim Glabisch <tim.glabisch@sensiolabs.de>
2016-03-15 23:16:56 +0100

    info on old php version

--- deptrac.php
+++ deptrac.php
@@ -9,6 +9,12 @@ use Symfony\Component\Console\Application;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
+
+if (!version_compare(phpversion(), "5.5.9", '>=')) {
+    echo 'Required at least PHP version 5.5.9, your version: ' . PHP_VERSION . "\n";
+    die(1);
+}
+
 (new XmlFileLoader($container = new ContainerBuilder(), new FileLocator(__DIR__)))->load(__DIR__. '/services.xml');
 $container
     ->addCompilerPass(new OutputFormatterPass())
