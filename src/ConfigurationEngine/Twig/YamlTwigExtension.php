83771e75aeb831973595f7fc02706bfab96829cd
Tim Glabisch <tim.glabisch@sensiolabs.de>
2016-05-09 21:08:13 +0200

    more robust configuration

--- src/ConfigurationEngine/Twig/YamlTwigExtension.php
+++ src/ConfigurationEngine/Twig/YamlTwigExtension.php
@@ -14,6 +14,10 @@ class YamlTwigExtension extends \Twig_Extension
         return [
             new \Twig_SimpleFilter(
                 'yaml', function($data) {
+                    if (is_string($data) && strpos(trim($data), "\"!!deptrac_") === 0) {
+                        return trim($data);
+                    }
+
                     return json_encode('!!deptrac_'.$data);
                 },
                 ['is_safe' => ['html']]
