b0669200785a61b27ab4a25a116c9f381e49a460
Simon Mönch <simonmoench@googlemail.com>
2018-01-12 16:02:13 +0100

    Drop PHP <7.0 and HHVM support + upgrade to symfony 3.4 LTS (#138)
    * Drop PHP <7.0 and HHVM support + upgrade to symfony 3.4 LTS
    
    * fix php version requirement

--- deptrac.php
+++ deptrac.php
@@ -9,18 +9,16 @@ use Symfony\Component\Console\Application;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
-
-if (!version_compare(phpversion(), "5.5.9", '>=')) {
-    echo 'Required at least PHP version 5.5.9, your version: ' . PHP_VERSION . "\n";
+if (PHP_VERSION_ID < 70000) {
+    echo 'Required at least PHP version 7.0.0, your version: '.PHP_VERSION."\n";
     die(1);
 }
 
-(new XmlFileLoader($container = new ContainerBuilder(), new FileLocator(__DIR__)))->load(__DIR__. '/services.xml');
+(new XmlFileLoader($container = new ContainerBuilder(), new FileLocator(__DIR__)))->load(__DIR__.'/services.xml');
 $container
     ->addCompilerPass(new OutputFormatterPass())
     ->addCompilerPass(new CollectorPass())
-    ->compile()
-;
+    ->compile();
 
 $container->set('container', $container);
 
