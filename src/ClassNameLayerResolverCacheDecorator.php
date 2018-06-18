ffe975121994dfd0cf036955d5c4ba9633abb477
Jan Schädlich <jan.schaedlich@sensiolabs.de>
2018-06-18 14:14:05 +0200

    add cache for layer names

--- src/ClassNameLayerResolverCacheDecorator.php
+++ src/ClassNameLayerResolverCacheDecorator.php
@@ -5,29 +5,30 @@ namespace SensioLabs\Deptrac;
 class ClassNameLayerResolverCacheDecorator implements ClassNameLayerResolverInterface
 {
     private $classNameLayerResolver;
-    private $classLayerCache = [];
+
+    private $layerNamesByClassCache = [];
+    private $layerNamesCache = [];
 
     public function __construct(ClassNameLayerResolverInterface $classNameLayerResolver)
     {
         $this->classNameLayerResolver = $classNameLayerResolver;
     }
 
-    /**
-     * @param string $className
-     *
-     * @return string[]
-     */
     public function getLayersByClassName(string $className): array
     {
-        if (!isset($this->classLayerCache[$className])) {
-            $this->classLayerCache[$className] = $this->classNameLayerResolver->getLayersByClassName($className);
+        if (!isset($this->layerNamesByClassCache[$className])) {
+            $this->layerNamesByClassCache[$className] = $this->classNameLayerResolver->getLayersByClassName($className);
         }
 
-        return $this->classLayerCache[$className];
+        return $this->layerNamesByClassCache[$className];
     }
 
     public function getLayers(): array
     {
-        return $this->classNameLayerResolver->getLayers();
+        if (empty($this->layerNamesCache)) {
+            $this->layerNamesCache = $this->classNameLayerResolver->getLayers();
+        }
+
+        return $this->layerNamesCache;
     }
 }
