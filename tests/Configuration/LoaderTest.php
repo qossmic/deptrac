8c033634b60414048638d5a8d820533cee735408
Andreas Möller <am@localheinz.com>
2019-08-19 11:12:51 +0200

    Enhancement: Throw exception when configuration cannot be parsed as yaml

---
+++ tests/Configuration/LoaderTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\SensioLabs\Deptrac\Configuration;
+
+use PHPUnit\Framework\TestCase;
+use SensioLabs\Deptrac\Configuration\Exception\FileCannotBeParsedAsYamlException;
+use SensioLabs\Deptrac\Configuration\Exception\MissingFileException;
+use SensioLabs\Deptrac\Configuration\Loader;
+
+/**
+ * @covers \SensioLabs\Deptrac\Configuration\Loader
+ */
+final class LoaderTest extends TestCase
+{
+    public function testLoadThrowsMissingFileExceptionWhenFileDoesNotExist(): void
+    {
+        $file = __DIR__.'/../../examples/non-existent-file.yml';
+
+        $loader = new Loader();
+
+        self::expectException(MissingFileException::class);
+
+        $loader->load($file);
+    }
+
+    public function testLoadThrowsFileCannotBeParsedAsYamlExceptionWhenFileDoesNotContainYaml(): void
+    {
+        $file = __FILE__;
+
+        $loader = new Loader();
+
+        self::expectException(FileCannotBeParsedAsYamlException::class);
+
+        $loader->load($file);
+    }
+}
