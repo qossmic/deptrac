9d85c7a736c0fe6d20a4db1e51cf4bb5f6360ee2
smoench <simon.moench@sensiolabs.de>
2019-11-21 13:13:52 +0100

    upgrade to symfony 5.0

--- src/AstRunner/Event/AstFileSyntaxErrorEvent.php
+++ src/AstRunner/Event/AstFileSyntaxErrorEvent.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace SensioLabs\Deptrac\AstRunner\Event;
 
-use Symfony\Component\EventDispatcher\Event;
+use Symfony\Contracts\EventDispatcher\Event;
 
 class AstFileSyntaxErrorEvent extends Event
 {
