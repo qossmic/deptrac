f630eb4aad3c89b026ca8765654df057598cc2ef
smoench <simon.moench@sensiolabs.de>
2020-11-18 20:05:56 +0100

    use fixed lexer

--- src/AstRunner/AstParser/NikicPhpParser/ParserFactory.php
+++ src/AstRunner/AstParser/NikicPhpParser/ParserFactory.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 namespace SensioLabs\Deptrac\AstRunner\AstParser\NikicPhpParser;
 
+use PhpParser\Lexer;
 use PhpParser\Parser;
 
 class ParserFactory
 {
     public static function createParser(): Parser
     {
-        return (new \PhpParser\ParserFactory())->create(\PhpParser\ParserFactory::PREFER_PHP7);
+        return (new \PhpParser\ParserFactory())->create(\PhpParser\ParserFactory::PREFER_PHP7, new Lexer());
     }
 }
