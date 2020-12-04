7dea31216e4f6aba151cf8d98cfacdea78cc995b
smoench <simon.moench@sensiolabs.de>
2020-12-04 12:22:34 +0100

    don't scope symfony polyfill's

--- deptrac.php
+++ deptrac.php
@@ -3,18 +3,20 @@
 use Composer\XdebugHandler\XdebugHandler;
 use SensioLabs\Deptrac\Console\Application;
 
-require __DIR__.'/vendor/autoload.php';
-
 if (PHP_VERSION_ID < 70205) {
     echo 'Required at least PHP version 7.2.5, your version: '.PHP_VERSION."\n";
     exit(1);
 }
 
-$autoloaderInWorkingDirectory = getcwd().'/vendor/autoload.php';
+(static function (): void {
+    require_once __DIR__.'/vendor/autoload.php';
+})();
 
-if (is_file($autoloaderInWorkingDirectory)) {
-    require_once $autoloaderInWorkingDirectory;
-}
+(static function (): void {
+    if (\file_exists($autoload = getcwd().'/vendor/autoload.php')) {
+        include_once $autoload;
+    }
+})();
 
 $xdebug = new XdebugHandler('DEPTRAC', '--ansi');
 $xdebug->check();
--- scoper.inc.php
+++ scoper.inc.php
@@ -2,13 +2,26 @@
 
 declare(strict_types=1);
 
+use Isolated\Symfony\Component\Finder\Finder;
+
+$polyfillsBootstrap = Finder::create()
+    ->files()
+    ->in(__DIR__ . '/vendor/symfony/polyfill-*')
+    ->name('bootstrap.php');
+
 return [
     'prefix' => null,                       // string|null
     'finders' => [],                        // Finder[]
     'patchers' => [],                       // callable[]
-    'files-whitelist' => [],                // string[]
+    'files-whitelist' => array_map(
+        static function ($file) {
+            return $file->getPathName();
+        },
+        iterator_to_array($polyfillsBootstrap)
+    ),
     'whitelist' => [
-        'SensioLabs\Deptrac\*',
+        'SensioLabs\\Deptrac\\*',
+        'Symfony\\Polyfill\\*',
     ],
     'whitelist-global-constants' => true,   // bool
     'whitelist-global-classes' => true,     // bool
