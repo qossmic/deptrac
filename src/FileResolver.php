40185029a2bb1de537fa65b9869db98934fc8c53
patrickkusebauch <patrick.kusebauch@gmail.com>
2021-06-25 21:58:21 +0200

    better message for FileResolver.php

--- src/FileResolver.php
+++ src/FileResolver.php
@@ -7,6 +7,7 @@ namespace Qossmic\Deptrac;
 use Iterator;
 use Qossmic\Deptrac\Configuration\Configuration;
 use SplFileInfo;
+use Symfony\Component\Finder\Exception\DirectoryNotFoundException;
 use Symfony\Component\Finder\Finder;
 
 class FileResolver
@@ -16,14 +17,18 @@ class FileResolver
      */
     public function resolve(Configuration $configuration): array
     {
-        $finder = (new Finder())
-            ->in($configuration->getPaths())
-            ->name('*.php')
-            ->files()
-            ->followLinks()
-            ->ignoreUnreadableDirs(true)
-            ->ignoreVCS(true)
-            ->notPath($configuration->getExcludeFiles());
+        try {
+            $finder = (new Finder())
+                ->in($configuration->getPaths())
+                ->name('*.php')
+                ->files()
+                ->followLinks()
+                ->ignoreUnreadableDirs(true)
+                ->ignoreVCS(true)
+                ->notPath($configuration->getExcludeFiles());
+        } catch (DirectoryNotFoundException $exception) {
+            throw new \RuntimeException('Invalid directory provided for `paths`. '.$exception->getMessage());
+        }
 
         $customFilterIterator = $finder->getIterator();
 
