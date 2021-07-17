0cbc5baef78408b6d5432a4ffdd6d56f691bea68
patrickkusebauch <patrick.kusebauch@gmail.com>
2021-07-17 20:09:08 +0200

    New collectors tests

---
+++ tests/Collector/SuperglobalCollectorTest.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Qossmic\Deptrac\Collector;
+
+use PHPUnit\Framework\TestCase;
+use Qossmic\Deptrac\AstRunner\AstMap;
+use Qossmic\Deptrac\Collector\FunctionNameCollector;
+use Qossmic\Deptrac\Collector\Registry;
+use Qossmic\Deptrac\Collector\SuperglobalCollector;
+
+final class SuperglobalCollectorTest extends TestCase
+{
+    public function dataProviderSatisfy(): iterable
+    {
+        yield [['names' => ['_GET', '_SESSION']], '_GET', true];
+        yield [['names' => ['_COOKIE']], '_POST', false];
+    }
+
+    public function testType(): void
+    {
+        self::assertEquals('superglobal', (new SuperglobalCollector())->getType());
+    }
+
+    /**
+     * @dataProvider dataProviderSatisfy
+     */
+    public function testSatisfy(array $configuration, string $name, bool $expected): void
+    {
+        $stat = (new SuperglobalCollector())->satisfy(
+            $configuration,
+            new AstMap\AstVariableReference(new AstMap\SuperGlobalName($name)),
+            $this->prophesize(AstMap::class)->reveal(),
+            $this->prophesize(Registry::class)->reveal()
+        );
+
+        self::assertEquals($expected, $stat);
+    }
+
+    public function testWrongRegexParam(): void
+    {
+        $this->expectException(\LogicException::class);
+
+        (new SuperglobalCollector())->satisfy(
+            ['Foo' => 'a'],
+            new AstMap\AstVariableReference(new AstMap\SuperGlobalName('_POST')),
+            $this->prophesize(AstMap::class)->reveal(),
+            $this->prophesize(Registry::class)->reveal()
+        );
+    }
+}
