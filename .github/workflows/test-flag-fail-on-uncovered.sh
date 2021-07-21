a490faad8d0b65b0bc22780e82aceacf85e1da6c
Denis Brumann <dbrumann@gmail.com>
2021-07-21 23:22:10 +0200

    Rename analyze to analyse for builds.

--- .github/workflows/test-flag-fail-on-uncovered.sh
+++ .github/workflows/test-flag-fail-on-uncovered.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-php deptrac.phar analyze examples/Uncovered.depfile.yaml --fail-on-uncovered --no-cache
+php deptrac.phar analyse examples/Uncovered.depfile.yaml --fail-on-uncovered --no-cache
 
 if [ $? -ne 1 ]; then
   exit 1;
