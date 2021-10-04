f0fe5838689f45f32636f2f45c764918da6fb482
Markus Staab <markus.staab@redaxo.de>
2021-10-04 16:47:58 +0200

    rewrote ternary

--- src/AstRunner/AstMap/FileName.php
+++ src/AstRunner/AstMap/FileName.php
@@ -18,10 +18,15 @@ final class FileName implements TokenName
     public function toString(): string
     {
         $wd = getcwd();
+        
+        if ($wd !== false && 0 === strpos($this->path, $wd) {
+            $wd = FileHelper::normalizePath($wd);
+            $path = substr($this->path, strlen($wd))
+        } else {
+            $path = $this->path;
+        }
 
-        $path = false !== $wd && 0 === strpos($this->path, $wd) ? substr($this->path, strlen($wd)) : $this->path;
-
-        return FileHelper::normalizePath($path);
+        return $path;
     }
 
     public function getFilepath(): string
