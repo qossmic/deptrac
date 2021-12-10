fcadae7efccae0b44db87b0ca9313b62cfcb894e
Denis Brumann <denis.brumann@qossmic.com>
2021-12-10 11:26:24 +0100

    Extract debug token command runner

---
+++ src/Console/Command/DebugTokenRunner.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Qossmic\Deptrac\Console\Command;
+
+use function natcasesort;
+use Qossmic\Deptrac\Configuration\Loader;
+use Qossmic\Deptrac\Console\Output;
+use Qossmic\Deptrac\TokenAnalyser;
+
+/**
+ * @internal Should only be used by DebugTokenCommand
+ */
+final class DebugTokenRunner
+{
+    private TokenAnalyser $analyser;
+    private Loader $loader;
+
+    public function __construct(TokenAnalyser $analyser, Loader $loader)
+    {
+        $this->analyser = $analyser;
+        $this->loader = $loader;
+    }
+
+    public function run(DebugTokenOptions $options, Output $output): void
+    {
+        $configuration = $this->loader->load($options->getConfigurationFile());
+
+        $layers = $this->analyser->analyse($configuration, $options->getToken());
+
+        natcasesort($layers);
+
+        $output->writeLineFormatted($layers);
+    }
+}
