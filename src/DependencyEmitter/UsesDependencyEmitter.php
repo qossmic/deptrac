d23e69c5937e24b9d323922438fbb91be7f507d3
Matthias Devlamynck <matthias.devlamynck@mailoo.org>
2022-01-01 20:48:33 +0100

    Fix 758
    Prevent namespaces from being added to use dependencies. Namespace are
    detected by looking if the used FQDN is a prefix of some class FQDN.

--- src/DependencyEmitter/UsesDependencyEmitter.php
+++ src/DependencyEmitter/UsesDependencyEmitter.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Qossmic\Deptrac\DependencyEmitter;
 
 use Qossmic\Deptrac\AstRunner\AstMap;
+use Qossmic\Deptrac\AstRunner\AstMap\AstDependency;
 use Qossmic\Deptrac\Dependency\Dependency;
 use Qossmic\Deptrac\Dependency\Result;
 
@@ -21,7 +22,9 @@ class UsesDependencyEmitter implements DependencyEmitterInterface
             $dependencies = $fileReference->getDependencies();
             foreach ($fileReference->getAstClassReferences() as $astClassReference) {
                 foreach ($dependencies as $emittedDependency) {
-                    if (AstMap\AstDependency::USE === $emittedDependency->getType()) {
+                    if (AstDependency::USE === $emittedDependency->getType() &&
+                        !$this->isNamespace($emittedDependency, $astMap)
+                    ) {
                         $dependencyResult->addDependency(
                             new Dependency(
                                 $astClassReference->getTokenName(),
@@ -33,5 +36,35 @@ class UsesDependencyEmitter implements DependencyEmitterInterface
                 }
             }
         }
+    }
+
+    protected function isNamespace(AstDependency $dependency, AstMap $astMap): bool
+    {
+        $dependencyFQDN = $dependency->getTokenName()->toString();
+
+        $functionReferences = array_merge(...array_values(array_map(
+            static function ($file) { return $file->getFunctionReferences(); },
+            $astMap->getAstFileReferences()
+        )));
+        $references = array_merge($astMap->getAstClassReferences(), $functionReferences);
+
+        $isFQDN = false;
+        $isNamespace = false;
+
+        foreach ($references as $reference) {
+            $referenceFQDN = $reference->getTokenName()->toString();
+
+            if ($referenceFQDN === $dependencyFQDN) {
+                $isFQDN = true;
+            }
+
+            if (0 === strpos($referenceFQDN, $dependencyFQDN, 0) &&
+                $referenceFQDN !== $dependencyFQDN
+            ) {
+                $isNamespace = true;
+            }
+        }
+
+        return $isNamespace && !$isFQDN;
     }
 }
