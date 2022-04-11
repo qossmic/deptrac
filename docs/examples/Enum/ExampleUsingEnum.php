c81f84f1bf56c3862fb80b650ef2138fd98abf2c
Denis Brumann <denis.brumann@qossmic.com>
2022-04-11 19:26:35 +0200

    Add example and test for enums
    Fixes #842

---
+++ docs/examples/Enum/ExampleUsingEnum.php
@@ -0,0 +1,7 @@
+<?php declare(strict_types=1);
+
+class ExampleUsingEnum
+{
+    private MyBackedEnum $backedEnum;
+    private MyEnum $enum;
+}
---
+++ docs/examples/Enum/MyBackedEnum.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types=1);
+
+enum MyBackedEnum: int
+{
+    case My = 100;
+    case Backed = 200;
+    case Enum = 300;
+}
