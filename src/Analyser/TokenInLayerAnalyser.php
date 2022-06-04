542e85542895b905c73ca338f8946e3adcb03a4f
patrickkusebauch <patrick.kusebauch@gmail.com>
2022-06-04 19:00:35 +0200

    Improved Debug commands

--- src/Analyser/TokenInLayerAnalyser.php
+++ src/Analyser/TokenInLayerAnalyser.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Qossmic\Deptrac\Analyser;
 
+use Qossmic\Deptrac\Dependency\DependencyResolver;
 use Qossmic\Deptrac\Dependency\TokenResolver;
 use Qossmic\Deptrac\Layer\LayerResolverInterface;
 use function array_values;
@@ -16,14 +17,31 @@ class TokenInLayerAnalyser
     private TokenResolver $tokenResolver;
     private LayerResolverInterface $layerResolver;
 
+    /**
+     * @var array<string>
+     */
+    private array $tokenTypes;
+
     public function __construct(
         AstMapExtractor $astMapExtractor,
         TokenResolver $tokenResolver,
-        LayerResolverInterface $layerResolver
+        LayerResolverInterface $layerResolver,
+        array $config
     ) {
         $this->astMapExtractor = $astMapExtractor;
         $this->tokenResolver = $tokenResolver;
         $this->layerResolver = $layerResolver;
+        $emitters = array_merge(DependencyResolver::DEFAULT_EMITTERS, $config);
+        $this->tokenTypes = array_filter(
+            array_map(
+                static function (string $emitterType): ?string {
+                    $tokenType = TokenType::tryFromEmitterType($emitterType);
+
+                    return null === $tokenType ? null : $tokenType->value;
+                },
+                $emitters['types']
+            )
+        );
     }
 
     /**
@@ -34,17 +52,34 @@ class TokenInLayerAnalyser
         $astMap = $this->astMapExtractor->extract();
 
         $matchingTokens = [];
-        foreach ($astMap->getClassLikeReferences() as $classReference) {
-            $classToken = $this->tokenResolver->resolve($classReference->getToken(), $astMap);
-            if (in_array($layer, $this->layerResolver->getLayersForReference($classToken, $astMap), true)) {
-                $matchingTokens[] = $classToken->getToken()->toString();
+
+        if (in_array(TokenType::CLASS_LIKE, $this->tokenTypes, true)) {
+            foreach ($astMap->getClassLikeReferences() as $classReference) {
+                $classToken = $this->tokenResolver->resolve($classReference->getToken(), $astMap);
+                if (in_array($layer, $this->layerResolver->getLayersForReference($classToken, $astMap), true)) {
+                    $matchingTokens[] = $classToken->getToken()
+                        ->toString();
+                }
             }
         }
 
-        foreach ($astMap->getFunctionLikeReferences() as $functionReference) {
-            $functionToken = $this->tokenResolver->resolve($functionReference->getToken(), $astMap);
-            if (in_array($layer, $this->layerResolver->getLayersForReference($functionToken, $astMap), true)) {
-                $matchingTokens[] = $functionToken->getToken()->toString();
+        if (in_array(TokenType::FUNCTION, $this->tokenTypes, true)) {
+            foreach ($astMap->getFunctionLikeReferences() as $functionReference) {
+                $functionToken = $this->tokenResolver->resolve($functionReference->getToken(), $astMap);
+                if (in_array($layer, $this->layerResolver->getLayersForReference($functionToken, $astMap), true)) {
+                    $matchingTokens[] = $functionToken->getToken()
+                        ->toString();
+                }
+            }
+        }
+
+        if (in_array(TokenType::FILE, $this->tokenTypes, true)) {
+            foreach ($astMap->getFileReferences() as $fileReference) {
+                $fileToken = $this->tokenResolver->resolve($fileReference->getToken(), $astMap);
+                if (in_array($layer, $this->layerResolver->getLayersForReference($fileToken, $astMap), true)) {
+                    $matchingTokens[] = $fileToken->getToken()
+                        ->toString();
+                }
             }
         }
 
