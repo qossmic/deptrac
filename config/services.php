a8902832c5cdefd63d680d0d663d6cadc18c1489
Denis Brumann <denis.brumann@qossmic.com>
2022-08-05 15:51:54 +0200

    Revert b3ef802

--- config/services.php
+++ config/services.php
@@ -86,7 +86,6 @@ use Qossmic\Deptrac\Supportive\OutputFormatter\GraphVizOutputHtmlFormatter;
 use Qossmic\Deptrac\Supportive\OutputFormatter\GraphVizOutputImageFormatter;
 use Qossmic\Deptrac\Supportive\OutputFormatter\JsonOutputFormatter;
 use Qossmic\Deptrac\Supportive\OutputFormatter\JUnitOutputFormatter;
-use Qossmic\Deptrac\Supportive\OutputFormatter\MermaidJSOutputFormatter;
 use Qossmic\Deptrac\Supportive\OutputFormatter\TableOutputFormatter;
 use Qossmic\Deptrac\Supportive\OutputFormatter\XMLOutputFormatter;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
@@ -358,9 +357,6 @@ return static function (ContainerConfigurator $container): void {
         ->tag('output_formatter');
     $services
         ->set(CodeclimateOutputFormatter::class)
-        ->tag('output_formatter');
-    $services
-        ->set(MermaidJSOutputFormatter::class)
         ->tag('output_formatter');
 
     /*
