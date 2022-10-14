607b2ed9d5d16021547dba26c8f6a756612622e9
Gennadi McKelvey <gennadi.mckelvey@qossmic.com>
2022-10-14 16:17:53 +0200

    only export values if set

--- src/Supportive/Config/DeptracConfig.php
+++ src/Supportive/Config/DeptracConfig.php
@@ -12,13 +12,14 @@ final class DeptracConfig implements ConfigBuilderInterface
 {
     private bool $ignoreUncoveredInternalClasses = true;
     private bool $useRelativePathFromDepfile = true;
-    private array $paths = ['src'];
 
+    private array $paths = [];
     private array $layers = [];
     private array $formatters = [];
     private array $rulesets = [];
-    private array $analyser = [EmitterType::CLASS_TOKEN, EmitterType::USE_TOKEN];
+    private array $analyser = [];
     private array $skipViolations = [];
+    private array $excludeFiles = [];
 
     public function analyser(EmitterType ...$types): self
     {
@@ -59,17 +60,40 @@ final class DeptracConfig implements ConfigBuilderInterface
 
     public function toArray(): array
     {
-        return [
-            'paths' => $this->paths,
-            'exclude_files' => [],
-            'layers' => array_map(static fn (LayerConfig $layerConfig) => $layerConfig->toArray(), $this->layers),
-            'ruleset' => array_map(static fn (RulesetConfig $rulesetConfig) => $rulesetConfig->toArray(), $this->rulesets),
-            'skip_violations' => $this->skipViolations,
-            'formatters' => $this->formatters,
-            'analyser' => ['types' => array_map(static fn (EmitterType $emitterType) => $emitterType->value, $this->analyser)],
-            'ignore_uncovered_internal_classes' => $this->ignoreUncoveredInternalClasses,
-            'use_relative_path_from_depfile' => $this->useRelativePathFromDepfile,
-        ];
+        $config = [];
+
+        if ([] !== $this->paths) {
+            $config['paths'] = $this->paths;
+        }
+
+        if ([] !== $this->excludeFiles) {
+            $config['exclude_files'] = $this->excludeFiles;
+        }
+
+        if ([] !== $this->layers) {
+            $config['layers'] = array_map(static fn (LayerConfig $layerConfig) => $layerConfig->toArray(), $this->layers);
+        }
+
+        if ([] !== $this->rulesets) {
+            $config['ruleset'] = array_map(static fn (RulesetConfig $rulesetConfig) => $rulesetConfig->toArray(), $this->rulesets);
+        }
+
+        if ([] !== $this->skipViolations) {
+            $config['skip_violations'] = $this->skipViolations;
+        }
+
+        if ([] !== $this->formatters) {
+            $config['formatters'] = $this->formatters;
+        }
+
+        if ([] !== $this->skipViolations) {
+            $config['analyser']['types'] = array_map(static fn (EmitterType $emitterType) => $emitterType->value, $this->analyser);
+        }
+
+        $config['ignore_uncovered_internal_classes'] = $this->ignoreUncoveredInternalClasses;
+        $config['use_relative_path_from_depfile'] = $this->useRelativePathFromDepfile;
+
+        return $config;
     }
 
     public function getExtensionAlias(): string
