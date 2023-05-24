38cbb83b1c053b2da0786da9e57b691da1009956
Gennadi McKelvey <gennadijanzen@icloud.com>
2023-05-24 12:57:59 +0200

    only read file once

--- src/Supportive/File/FileReader.php
+++ src/Supportive/File/FileReader.php
@@ -13,10 +13,8 @@ final class FileReader
      */
     public static function read(string $fileName): string
     {
-        if (!is_file($fileName)) {
-            throw CouldNotReadFileException::fromFilename($fileName);
-        }
         $contents = @file_get_contents($fileName);
+
         if (false === $contents) {
             throw CouldNotReadFileException::fromFilename($fileName);
         }
