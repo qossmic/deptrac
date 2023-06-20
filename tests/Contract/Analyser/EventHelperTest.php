0bc3c0fe3170593a38f1fb952d0a2a19ae235513
Gennadi McKelvey <gennadijanzen@icloud.com>
2023-06-20 17:38:51 +0200

    add tests for skipped violations that occure multiple times

--- tests/Contract/Analyser/EventHelperTest.php
+++ tests/Contract/Analyser/EventHelperTest.php
@@ -32,6 +32,14 @@ final class EventHelperTest extends TestCase
                 ClassLikeToken::fromFQCN('DependencyClass')->toString()
             )
         );
+
+        // also skipps multiple occurrences
+        self::assertTrue(
+            $helper->shouldViolationBeSkipped(
+                ClassLikeToken::fromFQCN('ClassWithOneDep')->toString(),
+                ClassLikeToken::fromFQCN('DependencyClass')->toString()
+            )
+        );
         self::assertFalse(
             $helper->shouldViolationBeSkipped(
                 ClassLikeToken::fromFQCN('ClassWithEmptyDeps')->toString(),
@@ -73,6 +81,13 @@ final class EventHelperTest extends TestCase
         ];
         $helper = new EventHelper($configuration, new LayerProvider([]));
 
+        self::assertTrue(
+            $helper->shouldViolationBeSkipped(
+                ClassLikeToken::fromFQCN('ClassWithOneDep')->toString(),
+                ClassLikeToken::fromFQCN('DependencyClass')->toString()
+            )
+        );
+        // also skipps multiple occurrences
         self::assertTrue(
             $helper->shouldViolationBeSkipped(
                 ClassLikeToken::fromFQCN('ClassWithOneDep')->toString(),
