9213d777bceedd488a3a0fcb702a4fe046e823dc
Gennadi McKelvey <gennadijanzen@icloud.com>
2023-11-23 11:05:57 +0100

    also copy deptrac.yaml
    add debug env

--- build-deptrac.sh
+++ build-deptrac.sh
@@ -1,11 +1,12 @@
 rm -rf deptrac-build
 composer install -a --no-dev
 
-php -dmemory_limit=-1 build/php-scoper.phar add-prefix -c scoper.inc.php --working-dir . \
-    config internal src vendor deptrac deptrac.config.php deptrac.php \
-    --force
+php -dmemory_limit=-1 build/php-scoper.phar add-prefix \
+    -c scoper.inc.php --working-dir . \
+    config internal src vendor deptrac deptrac.config.php deptrac.php
 
 cp composer.* deptrac-build/
+cp deptrac.yaml deptrac-build/
 
 cd deptrac-build && composer dump -a --dev
 
